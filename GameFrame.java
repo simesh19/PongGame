a4156a4e9d45c3add0b0eefa71800d67c0389618
simesh19 <simeshjunghare@gmail.com>
2023-03-25 20:11:53 +0530

    commit

---
+++ GameFrame.java
@@ -0,0 +1,21 @@
+package PongGame;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+
+	GamePanel1 panel;
+	
+	GameFrame(){
+		panel = new GamePanel1();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
